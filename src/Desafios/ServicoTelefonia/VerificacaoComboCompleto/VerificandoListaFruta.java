8be65dc76f47e75bbe99be0bc49acc795f491857
Miguel <miguel.bezerra1@gmail.com>
2024-08-27 11:44:29 -0300

    Implementa VerificandoListaFruta

---
+++ src/Desafios/ServicoTelefonia/VerificacaoComboCompleto/VerificandoListaFruta.java
@@ -0,0 +1,35 @@
+package Desafios.ServicoTelefonia.VerificacaoComboCompleto;
+
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class VerificandoListaFruta {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> setFrutas = new HashSet<>();
+        setFrutas.add("Banana");
+        setFrutas.add("Melancia");
+        setFrutas.add("Uva");
+        setFrutas.add("Morango");
+        setFrutas.add("Maçã");
+
+        String frutasFormatadas = setFrutas.stream().collect(Collectors.joining(", ", "", ""));
+
+        System.out.println("Quais frutas você vai comprar?");
+        System.out.println(frutasFormatadas);
+        String comprarFrutas = scanner.nextLine().trim();
+
+        String[] frutasParaComprar = comprarFrutas.split(",");
+
+        Set<String> carrinho = new HashSet<>();
+        for ( String fruta : frutasParaComprar) {
+            carrinho.add(fruta.trim());
+
+        }
+        System.out.println(carrinho);
+    }
+}
