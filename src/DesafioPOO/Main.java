76e1a2939c5679ce98dbce50ec8f096dd61cfbe9
Miguel <miguel.bezerra1@gmail.com>
2024-08-27 21:54:55 -0300

    Implementa DesafioPo - Falta Completar

---
+++ src/DesafioPOO/Main.java
@@ -0,0 +1,15 @@
+package DesafioPOO;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso de Java focado em back-end");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Curso de Python focado em Ciência de Dados");
+        curso2.setCargaHoraria(13);
+    }
+}
