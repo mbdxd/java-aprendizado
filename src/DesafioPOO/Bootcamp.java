df9b7e3705c39ad80c6fc6fb84227dbd224b1b4c
Miguel <miguel.bezerra1@gmail.com>
2024-08-29 14:50:58 -0300

    Atualiza Bootcamp

--- src/DesafioPOO/Bootcamp.java
+++ src/DesafioPOO/Bootcamp.java
@@ -1,10 +1,15 @@
 package DesafioPOO;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class Bootcamp {
    private String nome;
    private String descricao;
-   private LocalDate dataInicial;
+   private final LocalDate dataInicial = LocalDate.now();
+   private final LocalDate dataFinal = dataInicial.plusDays(45);
+   private Set<Dev> devsInscritos = new HashSet<>();
+   private Set<Conteudo> conteudos = new LinkedHashSet<>();
 }
